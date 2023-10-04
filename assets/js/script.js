a85b8bbb12fa73e294d42b4e8a28010b4c57bd1b
khalid sodal <sodalkhalid10@gmail.com>
2023-10-04 15:54:20 +0000

    Set questions and answers for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,8 @@ beginButton.addEventListener('click', beginGame);
 function beginGame() {
     console.log('start')
     beginButton.classList.add('hide')
+    qstnContainer.classList.remove('hide')
+    nextQuestion()
 
 }
 
@@ -20,3 +22,58 @@ function nextQuestion() {
 function answerSelect() {
 
 }
+
+let questions = [
+    {
+        question: "What year was Man Utd FC founded?",
+        answers:[
+            {text: '1878', correct: true},
+            {text: '1978', correct: false},
+            {text: '1889', correct: false},
+            {text: '1901', correct: false}
+
+        ]
+        
+    },
+    {
+        question: "What is the name of Man Utd's home ground?",
+        answers:[
+            {text: 'Wembley', correct: false},
+            {text: 'The Emirates', correct: false},
+            {text: 'Old Trafford', correct: true},
+            {text: 'Stamford Bridge', correct: false}
+
+        ]
+    },
+    {
+        question: "Who did Man Utd beat in the 2008 Champions League final?",
+        answers:[
+            {text: 'Arsenal', correct: false},
+            {text: 'Chelsea', correct: true},
+            {text: 'Liverpool', correct: false},
+            {text: 'Real Madrid', correct: false}
+
+        ]
+    },
+    {
+        question: "Who is/was Man Utd most successful manager ever?",
+        answers:[
+            {text: 'Sir Matt Busby', correct: false},
+            {text: 'David Moyes', correct: false},
+            {text: 'Jose Mourinho', correct: false},
+            {text: 'Sir Alex Ferguson', correct: true}
+
+        ]
+    },
+    {
+        question: "In what year was Old Trafford built?",
+        answers:[
+            {text: '1896', correct: false},
+            {text: '1910', correct: true},
+            {text: '1995', correct: false},
+            {text: '1936', correct: false}
+
+        ]
+    }
+    
+]
